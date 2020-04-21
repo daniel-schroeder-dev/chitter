bcc425cc920a0865f9e02b47cf873c28ef8cd15a
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-20 19:47:25 -0500

    Add normalize.css, begin setting up PageTitleBar styling.

--- react-ui/src/components/PageTitleBar/PageTitleBar.js
+++ react-ui/src/components/PageTitleBar/PageTitleBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useRouteMatch } from 'react-router-dom';
 
+import profilePic from '../../fixtures/profile-pic.jpg';
+
 import './PageTitleBar.css';
 
 function PageTitleBar(props) {
@@ -8,8 +10,8 @@ function PageTitleBar(props) {
   const match = useRouteMatch();
 
   return (
-    <header>
-      <img src="" alt="" />
+    <header className="page-title-bar">
+      <img className="page-title-bar__user-avatar" src={profilePic} alt="user" />
       {match.params[0]}
     </header>
   );
