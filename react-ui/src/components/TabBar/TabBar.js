abcf8977d65808b6e2466aac4550adcc2a278039
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-21 18:47:45 -0500

    Finish responsive styling for TabBar and PageTitleBar.

--- react-ui/src/components/TabBar/TabBar.js
+++ react-ui/src/components/TabBar/TabBar.js
@@ -15,14 +15,14 @@ function TabBar(props) {
   return (
     <footer className="tab-bar">
       <NavLink className="tab-bar__logo-link" to="/home"><img className="tab-bar__logo" src={logo} alt="logo" /></NavLink>
-      <NavLink to="/home"><FontAwesomeIcon className="tab-bar__icon" icon={faHome} /></NavLink>
-      <NavLink to="/explore"><FontAwesomeIcon className="tab-bar__icon" icon={faSearch} /></NavLink>
-      <NavLink to="/notifications"><FontAwesomeIcon className="tab-bar__icon" icon={faBell} /></NavLink>
-      <NavLink to="/messages"><FontAwesomeIcon className="tab-bar__icon" icon={faEnvelope} /></NavLink>
-      <NavLink to="/bookmarks" className="hide-sm-screen"><FontAwesomeIcon className="tab-bar__icon hide-sm-screen" icon={faBookmark} /></NavLink>
-      <NavLink to={`/${username}/lists`} className="hide-sm-screen"><FontAwesomeIcon className="tab-bar__icon hide-sm-screen" icon={faClipboard} /></NavLink>
-      <NavLink to={`/${username}`} className="hide-sm-screen"><img className="tab-bar__user-avatar" src={profilePic} alt="user" /></NavLink>
-      <FontAwesomeIcon className="tab-bar__icon hide-sm-screen" icon={faStroopwafel} />
+      <NavLink className="tab-bar__nav-link" to="/home"><span className="tab-bar__icon__wrapper"><FontAwesomeIcon className="tab-bar__icon" icon={faHome} /></span><h3 className="tab-bar__nav-link-text hide-m-screen">Home</h3></NavLink>
+      <NavLink className="tab-bar__nav-link" to="/explore"><span className="tab-bar__icon__wrapper"><FontAwesomeIcon className="tab-bar__icon" icon={faSearch} /></span><h3 className="tab-bar__nav-link-text hide-m-screen">Explore</h3></NavLink>
+      <NavLink className="tab-bar__nav-link" to="/notifications"><span className="tab-bar__icon__wrapper"><FontAwesomeIcon className="tab-bar__icon" icon={faBell} /></span><h3 className="tab-bar__nav-link-text hide-m-screen">Notifications</h3></NavLink>
+      <NavLink className="tab-bar__nav-link" to="/messages"><span className="tab-bar__icon__wrapper"><FontAwesomeIcon className="tab-bar__icon" icon={faEnvelope} /></span><h3 className="tab-bar__nav-link-text hide-m-screen">Messages</h3></NavLink>
+      <NavLink className="tab-bar__nav-link hide-sm-screen" to="/bookmarks"><span className="tab-bar__icon__wrapper"><FontAwesomeIcon className="tab-bar__icon hide-sm-screen" icon={faBookmark} /></span><h3 className="tab-bar__nav-link-text hide-m-screen">Bookmarks</h3></NavLink>
+      <NavLink className="tab-bar__nav-link hide-sm-screen" to={`/${username}/lists`}><span className="tab-bar__icon__wrapper"><FontAwesomeIcon className="tab-bar__icon hide-sm-screen" icon={faClipboard} /></span><h3 className="tab-bar__nav-link-text hide-m-screen">Lists</h3></NavLink>
+      <NavLink className="tab-bar__nav-link hide-sm-screen" to={`/${username}`}><span className="tab-bar__icon__wrapper"><img className="tab-bar__user-avatar" src={profilePic} alt="user" /></span><h3 className="tab-bar__nav-link-text hide-m-screen">Profile</h3></NavLink>
+      <NavLink className="tab-bar__nav-link hide-sm-screen" to={`/${username}/settings`}><span className="tab-bar__icon__wrapper"><FontAwesomeIcon className="tab-bar__icon hide-sm-screen" icon={faStroopwafel} /></span><h3 className="tab-bar__nav-link-text hide-m-screen">More</h3></NavLink>
 
     </footer>
   );
