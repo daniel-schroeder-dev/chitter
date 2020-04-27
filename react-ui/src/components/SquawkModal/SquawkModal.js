29ac0d9fb13f2cfe1d89c5950beb779c8d360967
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-26 22:11:24 -0500

    Make left arrow trigger back() navigation on SquawkModal.

--- react-ui/src/components/SquawkModal/SquawkModal.js
+++ react-ui/src/components/SquawkModal/SquawkModal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
@@ -7,10 +8,13 @@ import profilePic from '../../fixtures/profile-pic.jpg';
 import './SquawkModal.css';
 
 function SquawkModal(props) {
+
+  const history = useHistory();
+
   return (
     <section className="squawk-modal">
       <div className="squawk-modal__top-bar">
-        <FontAwesomeIcon className="squawk-modal__icon" icon={faArrowLeft} />
+        <FontAwesomeIcon className="squawk-modal__icon" icon={faArrowLeft} onClick={history.goBack} />
         <button className="squawk-modal__squawk-button">Squawk</button>
       </div>
       <div className="squawk-modal__main-content-area">
